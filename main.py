bafbb215c20499b524fe31cc9e64acef35e0ac6e
Eskil <eskped@gmail.com>
2022-10-13 13:18:55 +1100

    Added f1-score vizualitation

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn import metrics
 from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_validate
+import matplotlib.pyplot as plt
 
 
 class Solver:
@@ -123,23 +125,28 @@ class Solver:
         self.df_nominal_mean_imputed = nominal_data
 
     def multivariate_imputation(self):
-        numerical_data = self.df_numerical.copy()
-        imputer = IterativeImputer(max_iter=10, random_state=42)
-        imputed = imputer.fit_transform(numerical_data)
-        df_imputed = pd.DataFrame(imputed, columns=numerical_data.columns)
-        self.df_numerical_multivariate_imputed = df_imputed
+        try:
+            self.df_multivariate_imputed = pd.read_csv(
+                'df_multivariate_imputed.csv')
+        except:
+            numerical_data = self.df_numerical.copy()
+            imputer = IterativeImputer(max_iter=10, random_state=42)
+            imputed = imputer.fit_transform(numerical_data)
+            df_imputed = pd.DataFrame(imputed, columns=numerical_data.columns)
+            self.df_numerical_multivariate_imputed = df_imputed
 
-        nominal_data = self.df_nominal.copy()
-        imp = IterativeImputer(max_iter=10, random_state=42)
-        imp = imp.fit_transform(nominal_data)
-        df_imp = pd.DataFrame(imp, columns=nominal_data.columns)
-        self.df_nominal_multivariate_imputed = df_imp
+            nominal_data = self.df_nominal.copy()
+            imp = IterativeImputer(max_iter=10, random_state=42)
+            imp = imp.fit_transform(nominal_data)
+            df_imp = pd.DataFrame(imp, columns=nominal_data.columns)
+            self.df_nominal_multivariate_imputed = df_imp
 
-        data = self.df.copy()
-        imput = IterativeImputer(max_iter=10, random_state=42)
-        imput = imput.fit_transform(data)
-        df_imput = pd.DataFrame(imput, columns=data.columns)
-        self.df_multivariate_imputed = df_imput
+            data = self.df.copy()
+            imput = IterativeImputer(max_iter=10, random_state=42)
+            imput = imput.fit_transform(data)
+            df_imput = pd.DataFrame(imput, columns=data.columns)
+            self.df_multivariate_imputed = df_imput
+            df_imput.to_csv('df_multivariate_imputed.csv')
 
     def nearest_neighbour_imputation(self):
         # not implemented yet
@@ -165,6 +172,77 @@ class Solver:
         print(y_pred.shape)
         print(f1_score(y_test, y_pred, average=None))
 
+    def cross_validation(self, model, _X, _y, _cv):
+        # numerical_data = self.df_numerical_mean_imputed.copy()
+        # nominal_data = self.df_nominal_mean_imputed.copy()
+        # _X = pd.concat([numerical_data, nominal_data], axis=1)
+        # _y = self.df_target.copy()
+        # _cv = 5
+
+        _scoring = ['accuracy', 'precision', 'recall', 'f1']
+        results = cross_validate(estimator=model,
+                                 X=_X,
+                                 y=_y,
+                                 cv=_cv,
+                                 scoring=_scoring,
+                                 return_train_score=True)
+
+        return {"Training Accuracy scores": results['train_accuracy'],
+                "Mean Training Accuracy": results['train_accuracy'].mean()*100,
+                "Training Precision scores": results['train_precision'],
+                "Mean Training Precision": results['train_precision'].mean(),
+                "Training Recall scores": results['train_recall'],
+                "Mean Training Recall": results['train_recall'].mean(),
+                "Training F1 scores": results['train_f1'],
+                "Mean Training F1 Score": results['train_f1'].mean(),
+                "Validation Accuracy scores": results['test_accuracy'],
+                "Mean Validation Accuracy": results['test_accuracy'].mean()*100,
+                "Validation Precision scores": results['test_precision'],
+                "Mean Validation Precision": results['test_precision'].mean(),
+                "Validation Recall scores": results['test_recall'],
+                "Mean Validation Recall": results['test_recall'].mean(),
+                "Validation F1 scores": results['test_f1'],
+                "Mean Validation F1 Score": results['test_f1'].mean()
+                }
+
+    def plot_result(self, x_label, y_label, plot_title, train_data, val_data, cv):
+        plt.figure(figsize=(12, 6))
+        labels = []
+        for i in range(cv):
+            labels.append(f'{i}. Fold')
+        X_axis = np.arange(len(labels))
+        ax = plt.gca()
+        plt.ylim(0.40000, 1)
+        plt.bar(X_axis-0.2, train_data, 0.4, color='blue', label='Training')
+        plt.bar(X_axis+0.2, val_data, 0.4, color='red', label='Validation')
+        plt.title(plot_title, fontsize=30)
+        plt.xticks(X_axis, labels)
+        plt.xlabel(x_label, fontsize=14)
+        plt.ylabel(y_label, fontsize=14)
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    def model_training(self):
+        # numerical_data = self.df_numerical_median_imputed.copy()
+        # nominal_data = self.df_nominal_mean_imputed.copy()
+        # X = pd.concat([numerical_data, nominal_data], axis=1)
+        X = self.df_multivariate_imputed.copy()
+        encoded_y = self.df_target.copy()
+        decision_tree_model = DecisionTreeClassifier(criterion="entropy", min_samples_split=5, max_depth=7,
+                                                     random_state=0)
+
+        decision_tree_result = self.cross_validation(
+            decision_tree_model, X, encoded_y, 5)
+        print(decision_tree_result)
+
+        model_name = "Decision Tree"
+        self.plot_result(model_name,
+                         "F1",
+                         "F1 Scores in 5 Folds",
+                         decision_tree_result["Training F1 scores"],
+                         decision_tree_result["Validation F1 scores"], 5)
+
     def main(self):
 
         # Normalization methods
@@ -179,10 +257,12 @@ class Solver:
 
         # NaN imputation methods
         self.mean_and_median_imputation()
-        # self.multivariate_imputation()
+        self.multivariate_imputation()
         # self.nearest_neighbour_imputation()
 
-        self.decision_tree_classifier()
+        # self.decision_tree_classifier()
+
+        self.model_training()
 
 
 if __name__ == '__main__':
